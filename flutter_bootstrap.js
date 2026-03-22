d630a4ede9f90c42ec627225150d1744fd82708b
Lee Taeoh <taeoh0311@gmail.com>
2026-03-22 13:15:41 +0900

    LToBank v7.1: UI feedback (Pastel cards, Logo, Pending Approvals Banner)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4172657219"
+    serviceWorkerVersion: "3974777447"
   }
 });
