e70d02d49551c6511b5afe2a149ba6b9b66f3747
Lee Taeoh <taeoh0311@gmail.com>
2026-03-22 13:37:42 +0900

    LToBank v7.3: Add Logo to AppBar and Revert Simulator

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "426207807"
+    serviceWorkerVersion: "1366306244"
   }
 });
